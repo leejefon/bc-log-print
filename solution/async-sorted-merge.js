4f15c92f88c92ff25ae3247392cd3447aaa0811d
Jeff Lee <jeff@healthiq.com>
2017-08-14 22:41:24 -0700

    Sync solution for async problem

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,5 +1,25 @@
 'use strict'
 
-module.exports = (logSources, printer) => {
-	throw new Error('Not implemented yet!  That part is up to you!')
+const Promise = require('bluebird');
+
+module.exports = async (logSources, printer) => {
+	let topLogs = await Promise.map(logSources, async source => await source.popAsync());
+	while (!topLogs.every(log => !log)) {
+		const index = nextIndex(topLogs);
+		printer.print(topLogs[index]);
+		topLogs[index] = await logSources[index].popAsync();
+	}
+	printer.done();
+};
+
+function nextIndex(logs) {
+	let earliestDate = new Date();
+	let id = 0;
+	for (let i = 0; i < logs.length; i++) {
+		if (logs[i] && logs[i].date < earliestDate) {
+			earliestDate = logs[i].date;
+			id = i;
+		}
+	}
+	return id;
 }
