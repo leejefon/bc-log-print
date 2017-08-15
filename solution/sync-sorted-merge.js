a9234eb5bc6465de1ae70e325d428641698e62c4
Jeff Lee <jeff@healthiq.com>
2017-08-14 21:50:12 -0700

    Preliminary solution for sync print

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,5 +1,23 @@
 'use strict'
 
 module.exports = (logSources, printer) => {
-	throw new Error('Not implemented yet!  That part is up to you!')
+	let topLogs = logSources.map(source => source.pop());
+	while (!topLogs.every(log => !log)) {
+		const index = nextIndex(topLogs);
+		printer.print(topLogs[index]);
+		topLogs[index] = logSources[index].pop();
+	}
+	printer.done();
+};
+
+function nextIndex(logs) {
+	let earliestDate = new Date();
+	let id = 0;
+	for (let i = 0; i < logs.length; i++) {
+		if (logs[i] && logs[i].date < earliestDate) {
+			earliestDate = logs[i].date;
+			id = i;
+		}
+	}
+	return id;
 }
