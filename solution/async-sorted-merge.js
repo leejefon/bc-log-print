5b11d6587e19d9e0ff571499cece8ec8c889e81a
Jeff Lee <jeff@healthiq.com>
2017-08-15 14:16:52 -0700

    Minor code clean up for async

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,32 +1,9 @@
 'use strict'
 
-// const Promise = require('bluebird');
-//
-// module.exports = async (logSources, printer) => {
-// 	let topLogs = await Promise.map(logSources, async source => await source.popAsync());
-// 	while (!topLogs.every(log => !log)) {
-// 		const index = nextIndex(topLogs);
-// 		printer.print(topLogs[index]);
-// 		topLogs[index] = await logSources[index].popAsync();
-// 	}
-// 	printer.done();
-// };
-//
-// function nextIndex(logs) {
-// 	let earliestDate = new Date();
-// 	let id = 0;
-// 	for (let i = 0; i < logs.length; i++) {
-// 		if (logs[i] && logs[i].date < earliestDate) {
-// 			earliestDate = logs[i].date;
-// 			id = i;
-// 		}
-// 	}
-// 	return id;
-// }
-
 module.exports = (logSources, printer) => {
   const logsList = Array.apply(null, Array(logSources.length)).map(() => []);
   let finishedSourceCount = 0; // NOTE: Not the best idea to have a global var in async calls, but since each source has long enough logs, should be ok
+
   const subscription = events.subscribe('printLog', (obj) => {
     if (!obj.log) {
       finishedSourceCount++;
@@ -35,16 +12,14 @@ module.exports = (logSources, printer) => {
     logsList[obj.source].push(obj.log);
 
     const topLogFromSources = logsList.map(logs => logs[0]);
-
     if (topLogFromSources.filter(l => l).length + finishedSourceCount === logSources.length) {
       const source = nextIndex(topLogFromSources);
-      printer.print(
-        logsList[source].shift()
-      );
+      printer.print(logsList[source].shift());
     }
 
     if (finishedSourceCount === logSources.length) {
       printer.done();
+      subscription.remove();
     }
   });
 
@@ -79,10 +54,10 @@ const events = (() => {
   return {
     subscribe: (topic, listener) => {
       // Create the topic's object if not yet created
-      if(!hOP.call(topics, topic)) topics[topic] = [];
+      if (!hOP.call(topics, topic)) topics[topic] = [];
 
       // Add the listener to queue
-      var index = topics[topic].push(listener) -1;
+      const index = topics[topic].push(listener) - 1;
 
       // Provide handle back for removal of topic
       return {
@@ -93,7 +68,7 @@ const events = (() => {
     },
     publish: (topic, info) => {
       // If the topic doesn't exist, or there's no listeners in queue, just leave
-      if(!hOP.call(topics, topic)) return;
+      if (!hOP.call(topics, topic)) return;
 
       // Cycle through topics queue, fire!
       topics[topic].forEach((item) => {
@@ -102,3 +77,29 @@ const events = (() => {
     }
   };
 })();
+
+// Alternative Solution:
+//
+// const Promise = require('bluebird');
+//
+// module.exports = async (logSources, printer) => {
+// 	let topLogs = await Promise.map(logSources, async source => await source.popAsync());
+// 	while (!topLogs.every(log => !log)) {
+// 		const index = nextIndex(topLogs);
+// 		printer.print(topLogs[index]);
+// 		topLogs[index] = await logSources[index].popAsync();
+// 	}
+// 	printer.done();
+// };
+//
+// function nextIndex(logs) {
+// 	let earliestDate = new Date();
+// 	let id = 0;
+// 	for (let i = 0; i < logs.length; i++) {
+// 		if (logs[i] && logs[i].date < earliestDate) {
+// 			earliestDate = logs[i].date;
+// 			id = i;
+// 		}
+// 	}
+// 	return id;
+// }
